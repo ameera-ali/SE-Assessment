bd1e8d4ede01b0fa4c6515c994c34b3e5a5fc897
ameera-ali <ameera.ali@education.nsw.gov.au>
2024-05-26 12:27:33 +1000

    aligning window
    I attempted to centre all the buttons and titles to the middle of the window

--- Welcome_page.py
+++ Welcome_page.py
@@ -6,10 +6,16 @@ win = tk.Tk()
 style1= font.Font(size=25)
 style2 = font.Font(size=20)
 
+win.grid_rowconfigure(0, weight=1)
+win.grid_columnconfigure(0, weight=1)
 
 #Welcome Page:
 welcome_pg = Frame(win)
 welcome_pg.grid(row=0, column=0, sticky="nsew")
+
+welcome_pg.grid_rowconfigure(0, weight=1)
+welcome_pg.grid_rowconfigure(1, weight=1)
+welcome_pg.grid_columnconfigure(0, weight=1)
 
 welcome_lb = Label(welcome_pg, text="Recognising Types of Triangles", font=style1)
 welcome_lb.pack(pady=20)
@@ -20,6 +26,10 @@ welcome_btn.pack()
 #Options Page: 
 options_pg = Frame(win)
 options_pg.grid(row=0, column=0, sticky="nsew")
+
+options_pg.grid_rowconfigure(0, weight=1)
+options_pg.grid_rowconfigure(1, weight=1)
+options_pg.grid_columnconfigure(0, weight=1)
 
 options_lb = Label(options_pg, text="Options", font=style1)
 options_lb.pack(pady=30)
@@ -34,6 +44,10 @@ quiz_btn.pack()
 #Learning Triangles Page:
 learning_triangles = Frame(win)
 learning_triangles.grid(row=0, column=0, sticky="nsew")
+
+learning_triangles.grid_rowconfigure(0, weight=1)
+learning_triangles.grid_rowconfigure(1, weight=1)
+learning_triangles.grid_columnconfigure(0, weight=1)
 
 triangles_lb = Label(learning_triangles, text="Learning Triangles", font=style1)
 triangles_lb.pack(pady=50)
@@ -54,6 +68,10 @@ right_btn.pack()
 isosceles = Frame(win)
 isosceles.grid(row=0, column=0, sticky="nsew")
 
+isosceles.grid_rowconfigure(0, weight=1)
+isosceles.grid_rowconfigure(1, weight=1)
+isosceles.grid_columnconfigure(0, weight=1)
+
 isosceles_lb = Label(isosceles, text="The Isosceles Triangle", font=style1)
 isosceles_lb.pack()
 
@@ -63,6 +81,10 @@ back_btn2.pack()
 #Scalene Triangle Page:
 scalene = Frame(win)
 scalene.grid(row=0, column=0, sticky="nsew")
+
+scalene.grid_rowconfigure(0, weight=1)
+scalene.grid_rowconfigure(1, weight=1)
+scalene.grid_columnconfigure(0, weight=1)
 
 scalene_lb = Label(scalene, text="The Scalene Triangle", font=style1)
 scalene_lb.pack()
@@ -74,6 +96,10 @@ back_btn3.pack()
 equilateral = Frame(win)
 equilateral.grid(row=0, column=0, sticky="nsew")
 
+equilateral.grid_rowconfigure(0, weight=1)
+equilateral.grid_rowconfigure(1, weight=1)
+equilateral.grid_columnconfigure(0, weight=1)
+
 equilateral_lb = Label(equilateral, text="The Equilateral Triangle", font=style1)
 equilateral_lb.pack()
 
@@ -84,6 +110,10 @@ back_btn4.pack()
 right_angled = Frame(win)
 right_angled.grid(row=0, column=0, sticky="nsew")
 
+right_angled.grid_rowconfigure(0, weight=1)
+right_angled.grid_rowconfigure(1, weight=1)
+right_angled.grid_columnconfigure(0, weight=1)
+
 right_lb = Label(right_angled, text="The Right-Angled Triangle", font=style1)
 right_lb.pack()
 
@@ -93,6 +123,10 @@ back_btn5.pack()
 #Quiz Page:
 quiz_pg = Frame(win)
 quiz_pg.grid(row=0, column=0, sticky="nsew")
+
+quiz_pg.grid_rowconfigure(0, weight=1)
+quiz_pg.grid_rowconfigure(1, weight=1)
+quiz_pg.grid_columnconfigure(0, weight=1)
 
 quiz_lb = Label(quiz_pg, text="The Quiz", font=style1)
 quiz_lb.pack()
